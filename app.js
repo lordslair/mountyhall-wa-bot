02eb39773d6673edabaa3ccec63600f2e3a96554
Lordslair <5469390+lordslair@users.noreply.github.com>
2023-01-23 23:22:17 +0000

    Added ?mob

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const venom = require('venom-bot');
 const https = require('https');
+const querystring = require('querystring');
 const winston = require('winston');
 const { combine, timestamp, printf, colorize } = winston.format;
 
@@ -52,7 +53,7 @@ function start(client) {
     }
 
     if (message.body.startsWith("?troll")) {
-    // Here we are trying to catch TROLL requests //
+      // Here we are trying to catch TROLL requests //
       regexp_match = message.body.match(/^[?]troll (\d+)$/)
       logger.debug(`Command OK (${message})`)
 
@@ -113,6 +114,147 @@ function start(client) {
       else {
         logger.warn('RegEx KO');
       }
+    } else if (message.body.startsWith("?mob")) {
+      // Here we are trying to catch MONSTERS requests //
+      regexp_match = message.body.match(/^[?]mob (\d+)$/)
+      logger.debug(`Command OK (${message.body})`)
+
+      if (regexp_match) {
+        mob_id = parseInt(regexp_match[1], 10)
+        logger.debug(`[${mob_id}] RegExp OK`)
+        // we need a MH call first to have the NAME of the mob
+        var req = https.request({
+          'method': 'GET',
+          'hostname': 'games.mountyhall.com',
+          'path': `/mountyhall/View/MonsterView.php?ai_IDPJ=${mob_id}`,
+          'maxRedirects': 20
+        }, function (res) {
+          res.setEncoding('binary');
+          res.on("data", function (data) {
+            arr = data.split("\n")
+            for (var i = 0; i < arr.length; i++) {
+              if (arr[i].includes(mob_id) && arr[i].includes('mh_monstres')) {
+                logger.silly(arr[i])
+                // We grabbed a line with the mob_id inside
+                // Let's see if we can grab his name
+                regexp_mob_name = arr[i].match(/class="mh_monstres">un[e]? ([-'A-Za-zÀ-ÿ\s]*) \[([A-Za-zÀ-ÿ]*)\]/)
+                if (regexp_mob_name) {
+                  mob_name = regexp_mob_name[1]
+                  mob_age = regexp_mob_name[2]
+                  break
+                }
+              }
+            }
+
+            if (mob_name && mob_age) {
+              logger.debug(`[${mob_id}] RegExp OK: ${mob_name} [${mob_age}]`)
+            }
+            else {
+              message = `[${mob_id}] RegExp KO: Unable to grab name & age`
+              // CdM was not found in events, we answer nicely
+              logger.warn(message)
+              mob_data = message
+            }
+
+            // Now we have id, name, age
+            // Lets call the next API to grab monster data
+            var options = {
+              'method': 'POST',
+              'hostname': 'mz.mh.raistlin.fr',
+              'path': '/mz/getCaracMonstre.php',
+              'headers': {
+                'Content-Type': 'application/x-www-form-urlencoded'
+              },
+              'maxRedirects': 20
+            };
+
+            var req = https.request(options, function (res) {
+              var chunks = [];
+            
+              res.on("data", function (chunk) {
+                chunks.push(chunk);
+              });
+            
+              res.on("end", function (chunk) {
+                var body = Buffer.concat(chunks);
+                let json = JSON.parse(body);
+                
+                mob = json[0]
+                logger.silly(mob)
+                
+                // Level
+                if (mob.niv.min == mob.niv.max) {
+                  LVL_DATA = mob.niv.min
+                } else {
+                  LVL_DATA = `${mob.niv.min}-${mob.niv.max}`
+                }
+                // PV
+                if (mob.pv.min2) {
+                  PV_DATA = `${mob.pv.min2}-${mob.pv.max2} PV`
+                } else {
+                  PV_DATA = `${mob.pv.min}-${mob.pv.max} PV`
+                }
+                // Date blessure
+                if (mob.bless && mob.timegmt) {
+                  let dt = new Date((mob.timegmt + 3600) * 1000)
+                  // get current date
+                  // adjust 0 before single digit date
+                  let date = ("0" + dt.getDate()).slice(-2);
+                  // get current month
+                  let month = ("0" + (dt.getMonth() + 1)).slice(-2);
+                  // get time
+                  let time = `${dt.getHours()}:${dt.getMinutes()}`;
+                  PV_DATA = PV_DATA + ` (${mob.bless}% @${date}/${month} ${time})`
+                }
+                // Armure
+                ARM_DATA_P = `${mob.armP.min2 || mob.armP.min || 1}-${mob.armP.max2 || mob.armP.max}`
+                ARM_DATA_M = `${mob.armM.min2 || mob.armM.min || 1}-${mob.armM.max2 || mob.armM.max}`
+                ARM_DATA = `(P): ${ARM_DATA_P} | (M): ${ARM_DATA_M}`
+                // Esquive 
+                ESQ_DATA = `${mob.esq.min2 || mob.esq.min || 1}-${mob.esq.max2 || mob.esq.max}D6`
+                // Header
+                MOB_HEADER = `[${mob_id}] ${mob_name} [${mob_age}] (${LVL_DATA})`
+
+                mob_data = `*${MOB_HEADER}*\n` +
+                `*PVs*: ${PV_DATA}\n` +
+                `*ARM*: ${ARM_DATA}\n` +
+                `*ESQ*: ${ESQ_DATA}`;
+                logger.debug(mob_data)
+
+                client
+                  .sendText(message.from, mob_data)
+                  .then((result) => {
+                    logger.silly('Result: ', result); //return object success
+                  })
+                  .catch((erro) => {
+                    logger.error('Error when sending: ', erro); //return object error
+                  });
+              });
+            
+              res.on("error", function (error) {
+                console.error(error);
+              });
+            });
+            
+            var postData = querystring.stringify({
+              'l': `[{"index":1,"id":${mob_id},"nom":"${mob_name} [${mob_age}]"}]`
+            });
+            
+            req.write(postData);
+            
+            req.end();
+          });
+          res.on("error", function (error) {
+            logger.error(error);
+          });
+        });
+        req.end();
+      }
+      else {
+      logger.warn('RegEx KO - Monster ID not found');
+      }
+    } else {
+      logger.warn('RegEx KO - Command unknown');
     }
   });
 }
